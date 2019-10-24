85da5a834cb4b4526b27cbf11fcda6e66d38124e
Boris <boris@token.store>
2019-10-24 12:34:54 +0300

    Updates

---
+++ precache-manifest.c5b9e02ae7aaf3b755d2eedd5b910449.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe24b11a10e74982dcb834e3239d52b",
+    "url": "/text-formatter/index.html"
+  },
+  {
+    "revision": "bff20db886feb85ad5bf",
+    "url": "/text-formatter/static/css/main.cb033eea.chunk.css"
+  },
+  {
+    "revision": "59e3071f477ed241c554",
+    "url": "/text-formatter/static/js/2.86e5f444.chunk.js"
+  },
+  {
+    "revision": "bff20db886feb85ad5bf",
+    "url": "/text-formatter/static/js/main.51fe0ac1.chunk.js"
+  },
+  {
+    "revision": "fa6795e5b4e83f0e26e3",
+    "url": "/text-formatter/static/js/runtime-main.9a95d0d7.js"
+  },
+  {
+    "revision": "8eb3964adbb2ead3837302b814fd5c61",
+    "url": "/text-formatter/static/media/one-column.8eb3964a.svg"
+  },
+  {
+    "revision": "1cfd1aa3b4fe4bba10c132acea9985af",
+    "url": "/text-formatter/static/media/roboto-regular.1cfd1aa3.woff"
+  },
+  {
+    "revision": "2222f1fd23aa2c08af158311d680ac4a",
+    "url": "/text-formatter/static/media/roboto-regular.2222f1fd.woff2"
+  },
+  {
+    "revision": "858b4b89139fdbb367a7470373885c21",
+    "url": "/text-formatter/static/media/three-columns.858b4b89.svg"
+  },
+  {
+    "revision": "120f7e83dacec3545732813d3583a0e8",
+    "url": "/text-formatter/static/media/two-columns.120f7e83.svg"
+  }
+]);
